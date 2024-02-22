46f403e357dbb5df1c76a593f6aece1f6195b994
rahmatilloziyoviddinov <rahmatilloziyoviddinov@gmail.com>
2024-02-22 11:10:43 +0500

    finished

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ setInterval(() => {
     let currectTime = new Date();
 
     hrs.innerHTML = (currectTime.getHours() < 10 ? "0" : "") + currectTime.getHours();
-    min.innerHTML = currectTime.getMinutes();
-    sec.innerHTML = (currectTime.getSeconds()<10? "0" : "") + currectTime.getSeconds();
+    min.innerHTML = (currectTime.getMinutes() < 10 ? "0" : "") + currectTime.getMinuts();
+    sec.innerHTML = (currectTime.getSeconds() < 10 ? "0" : "") + currectTime.getSeconds();
 }, 0)
